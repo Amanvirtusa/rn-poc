f82aaceb3a2269e3da0365e302a37da95853bc6b
Sylwolveryn <sylwolveryn@gmail.com>
2023-11-30 10:43:59 +0100

    add scrollview

--- screens/CKOScreen.js
+++ screens/CKOScreen.js
@@ -1,4 +1,4 @@
-import {StyleSheet, View} from "react-native";
+import {StyleSheet, View, SafeAreaView, ScrollView} from "react-native";
 import * as React from "react";
 import {Cart, CheckoutButton, PaymentDetails, UserDetails} from "../components";
 import {paymentStore} from "../stores";
@@ -14,17 +14,26 @@ export function CKOScreen({navigation}) {
             borderColor: 'black',
             borderBottomWidth: StyleSheet.hairlineWidth,
             margin: 10,
+        },
+        container: {
+            flex: 1,
+        },
+        scrollView: {
+            backgroundColor: 'rgba(122, 122, 255, 0.15)',
+            marginHorizontal: 20,
         }
-    });
-    return (
-        <View style={{padding: 20}}>
-            <UserDetails/>
-            <View style={styles.lineStyle}/>
-            <Cart paymentStore={paymentStore}/>
-            <View style={styles.lineStyle}/>
-            <PaymentDetails paymentStore={paymentStore}/>
-            <View style={styles.lineStyle}/>
-            <CheckoutButton navigation={navigation}/>
-        </View>
+    })
+return (
+    <SafeAreaView style={styles.container}>
+            <ScrollView style={styles.scrollView}>
+                <UserDetails/>
+                <View style={styles.lineStyle}/>
+                <Cart paymentStore={paymentStore}/>
+                <View style={styles.lineStyle}/>
+                <PaymentDetails paymentStore={paymentStore}/>
+                <View style={styles.lineStyle}/>
+                <CheckoutButton navigation={navigation}/>
+            </ScrollView>
+        </SafeAreaView>
     );
 }
